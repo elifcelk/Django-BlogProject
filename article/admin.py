e4bb3d152a5a9bf1b4f8fcdb21f0d8f7a3a6de3c
elifclk444@gmail.com <elifclk444@gmail.com>
2021-08-19 01:40:33 +0300

    19.08.2021

--- article/admin.py
+++ article/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Article
 @admin.register(Article) #decoratör
-class ArticleAdmin(admin.ModelAdmin):  #admin panelini özelleştirmek için
+class ArticleAdmin(admin.ModelAdmin):  #admin panelini özelleştirmesi
     
-    list_display =["title","author","created_date"] #bu kısımlar da gözüksün
-    list_display_links =["title","created_date"] #bu kısımlarda link özelliği
-    search_fields = ["title"] #buna göre  arama özelliği
-    list_filter = ["created_date"] #bu özelliğe göre filtreleme
+    list_display =["title","author","created_date"] 
+    list_display_links =["title","created_date"] 
+    search_fields = ["title"] 
+    list_filter = ["created_date"] 
     class Meta:
-        model = Article #articleadmin classı ile article ı birleştirmiş oluyoruz.
+        model = Article #articleadmin classı ile article ı birleştirir
