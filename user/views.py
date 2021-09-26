3d8cb2513acf038b74ac7c1a786b681541d2656c
elifclk444@gmail.com <elifclk444@gmail.com>
2021-09-26 17:57:23 +0300

    26.09.2021

--- user/views.py
+++ user/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
 
 # Create your views here.
 
 def register(request):
-    return render(request,"register.html")
+
+    form = RegisterForm(request.POST or None) #POST ise bilgiler alınıp form oluşur GET ise boş form oluşur
+    if form.is_valid(): # clean fonksiyonu burada çağırılıyor.True/False olarak burada kontrol ediliyor.
+        username = form.cleaned_data.get("username")
+        password= form.cleaned_data.get("password") # clean fonksiyonunda parola doğrulaması hata vermezse sözlük yapısı şeklinde değerleri döndürüyorduk.Burada o değerleri alıyoruz.
+
+        newUser = User(username=username) #User modelinden obje oluşturup veritabanına kaydettik.
+        newUser.set_password(password) # password şifrelendi
+        newUser.save()
+        login(request,newUser) # kaydolduktan sonra sisteme otomatik olarak giriş yapmış oldu
+
+        messages.info(request,"Başarıyla kayıt oldunuz.")
+
+        return redirect("index")
+    context = {
+            "form" :form
+        }
+    return render(request,"register.html",context)
+    
+
+
 
 def loginUser(request):
     return render(request,"login.html")
