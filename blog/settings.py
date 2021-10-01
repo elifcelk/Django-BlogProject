2c66afd3e5b0b3a95fae7e884c757e0034fadb9a
elifclk444@gmail.com <elifclk444@gmail.com>
2021-10-01 16:00:14 +0300

    1.10.2021

--- blog/settings.py
+++ blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "article",
     "user",
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- user/views.py
+++ user/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm,LoginForm
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate,logout
 
 
 # Create your views here.
@@ -31,7 +31,30 @@ def register(request):
 
 
 def loginUser(request):
-    return render(request,"login.html")
+    form = LoginForm(request.POST or None) 
+
+    context = {
+        "form":form
+    }
+    
+    if form.is_valid(): # Form classındaki clean metodu otomatik olarak çalışır
+        username= form.cleaned_data.get("username")
+        password=form.cleaned_data.get("password")
+
+        user = authenticate(username = username,password = password)
+        if  user is None:
+            messages.info(request,"Kullanıcı Adı veya Parola Hatalı")
+            return render(request,"login.html",context)
+        
+        messages.success(request,"Başarıyla giriş yaptınız.")
+        login(request,user)
+        return redirect("index")
+    return render(request,"login.html",context)
+
+
+
 
 def logoutUser(request):
-    pass
+    logout(request)
+    messages.success(request,"Başarıyla çıkış yaptınız.")
+    return redirect("index")
